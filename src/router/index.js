065a699920b28657cca7d48e757fc33dca6b1055
jc <1144096387@qq.com>
2019-11-16 15:15:14 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,145 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // 路由懒加载
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
+
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/', // 首页
+    name: 'index',
+    component: () => import('@/views/index.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login', // 登陆
+    name: 'login',
+    component: () => import('@/views/login.vue')
+  },
+  {
+    path: '/regist', // 注册
+    name: 'regist',
+    component: () => import('@/views/regist/regist.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'index',
+        component: () => import('@/views/regist/registInputInfo.vue'),
+      },
+      {
+        path: 'registInputInfo',
+        name: 'registInputInfo',
+        component: () => import('@/views/regist/registInputInfo.vue'),
+      },
+      {
+        path: 'registSuccess',
+        name: 'registSuccess',
+        component: () => import('@/views/regist/registSuccess.vue'),
+      }
+    ]
+  },
+  {
+    path: '/goodsList', // 商品列表
+    name: 'goodsList',
+    component: () => import('@/views/goodsList.vue'),
+  },
+  {
+    path: '/goodsDetail', // 商品详情
+    name: 'goodsDetail',
+    component: () => import('@/views/goodsDetail.vue'),
+  },
+  {
+    path: '/addSuccess', // 加入成功
+    name: 'addSuccess',
+    component: () => import('@/views/addSuccess.vue'),
+  },
+  {
+    path: '/shoppingCart', // 购物车
+    name: 'shoppingCart',
+    component: () => import('@/views/shoppingCart.vue'),
+  },
+  {
+    path: '/pay', // 支付页面
+    name: 'pay',
+    component: () => import('@/views/pay.vue'),
+  },
+  {
+    path: '/paySuccess', // 支付成功页面
+    name: 'paySuccess',
+    component: () => import('@/views/paySuccess.vue'),
+  },
+  {
+    path: '/feedback', // 反馈页面
+    name: 'feedback',
+    component: () => import('@/views/feedback.vue'),
+  },
+  {
+    path: '/personalCenter', // 个人中心
+    name: 'personalCenter',
+    component: () => import('@/views/personalCenter/personalCenter.vue'),
+    children: [
+      {
+        path: '/',//个人中心-主页
+        name: 'index',
+        component: () => import('@/views/personalCenter/personalCenterMyOrder.vue'),
+      },
+      {
+        path: 'personalCenterMyAddress',//个人中心-我的地址
+        name: 'personalCenterMyAddress',
+        component: () => import('@/views/personalCenter/personalCenterMyAddress.vue'),
+      },
+      {
+        path: 'personalCenterAddAddress',//个人中心-添加地址
+        name: 'personalCenterAddAddress',
+        component: () => import('@/views/personalCenter/personalCenterAddAddress.vue'),
+      },
+      {
+        path: 'personalCenterMyOrder',//个人中心-我的订单
+        name: 'personalCenterMyOrder',
+        component: () => import('@/views/personalCenter/personalCenterMyOrder.vue'),
+      },
+      {
+        path: 'personalCenterMyShoppingCart',//个人中心-我的购物车
+        name: 'personalCenterMyShoppingCart',
+        component: () => import('@/views/personalCenter/personalCenterMyShoppingCart.vue'),
+      }
+    ]
+  },
+  // {
+  //   path: '/merchant',
+  //   name: 'Merchant',
+  //   component: Merchant
+  // }
 ]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+// 添加顶部进度条
+import ViewUI from 'view-design';
+Vue.use(ViewUI);
+router.beforeEach((to, from, next) => {
+  ViewUI.LoadingBar.start();
+  next();
+});
+router.afterEach(route => {
+  ViewUI.LoadingBar.finish();
+});
 
 export default router
---
+++ src/views/regist/registInputInfo.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="info-form">
+     <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80" >
+        <FormItem label="用户名" prop="name">
+            <i-input v-model="formValidate.name" clearable placeholder="请输入你的用户名"></i-input>
+        </FormItem>
+        <FormItem label="邮箱" prop="mail">
+            <i-input v-model="formValidate.mail" clearable placeholder="请输入你的邮箱"></i-input>
+        </FormItem>
+        <FormItem label="密码" prop="password">
+            <i-input type="password" v-model="formValidate.password" clearable placeholder="请输入你的密码"></i-input>
+        </FormItem>
+        <FormItem label="确认密码" prop="repassword">
+            <i-input type="password" v-model="formValidate.repassword" clearable placeholder="请再次输入你的密码"></i-input>
+        </FormItem>
+        <Button type="error" size="large" long @click="handleSubmit('formValidate')">注册</Button>
+    </Form>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapActions } from 'vuex';
+export default {
+  name: 'registInputInfo',
+  data () {
+    const validatePassCheck = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.formValidate.password) {
+        callback(new Error('两次输入的密码不一样'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      formValidate: {
+        name: '',
+        mail: '',
+        password: '',
+        repassword: ''
+      },
+      ruleValidate: {
+        name: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' }
+        ],
+        mail: [
+          { required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { type: 'email', message: '邮箱格式错误', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { type: 'string', min: 6, message: '密码长度不能小于6', trigger: 'blur' }
+        ],
+        repassword: [
+          { required: true,validator: validatePassCheck, trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    ...mapMutations(['SET_SIGN_UP_SETP']),
+    ...mapActions(['signUp']),
+    handleSubmit (name) {
+      const father = this;
+      // test
+      father.$Message.success('注册成功');
+      father.SET_SIGN_UP_SETP(1);
+      father.$router.push({ path: '/regist/registSuccess' });
+      // end: test
+      // this.$refs[name].validate((valid) => {
+      //   if (valid) {
+      //     const data = {
+      //       username: this.formValidate.name,
+      //       phone: this.$route.query.phone,
+      //       password: this.formValidate.password,
+      //       mail: this.formValidate.mail,
+      //       status: 0
+      //     };
+      //     this.signUp(data).then(data => {
+      //       if (data) {
+      //         father.$Message.success('注册成功');
+      //         father.SET_SIGN_UP_SETP(1);
+      //         father.$router.push({ path: '/regist/registSuccess' });
+      //       } else {
+      //         this.$Message.error('注册失败, 请重新注册');
+      //       }
+      //     });
+      //   } else {
+      //     this.$Message.error('注册失败, 请先填写完正确信息');
+      //   }
+      // });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.info-form {
+  width: 90% !important;
+  margin-top: 20px;
+}
+</style>
